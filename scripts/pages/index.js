a938deb847e949544b55b4f17b96788f4d03ed99
Laurent Bigey <laurent.bigey@outlook.fr>
2023-06-02 08:53:55 +0200

    ajout images et fetch

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,19 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        const response = await fetch("data/photographers.json");
+        let photographers = "";
+
+        if (response.ok) { // if HTTP-status is 200-299
+            // get the response body
+            photographers = await response.json();
+        }
+        else {
+            alert("HTTP-Error: " + response.status);
+        }
+        console.log(photographers)
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        return (photographers)
     }
 
     async function displayData(photographers) {
