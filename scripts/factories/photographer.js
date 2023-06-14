d86d17d89c714f1045a69d63e76b05e7d3f91f08
Laurent Bigey <laurent.bigey@outlook.fr>
2023-06-14 20:43:06 +0200

    css page accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,33 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.setAttribute("aria-label", "Fiche photographe");
+        const a = document.createElement('a');
+        a.setAttribute("aria-label", "Voir les détails");
+        a.setAttribute("href", "photographer.html?id="+id);
+        const imgContainer = document.createElement( 'div' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "Photo de "+name);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const p1 = document.createElement( 'p' );
+        p1.textContent = city+', '+country;
+        const p2 = document.createElement( 'p' );
+        p2.textContent = tagline;
+        const p3 = document.createElement( 'p' );
+        p3.textContent = price+'€/jour';
+        article.appendChild(a);
+        a.appendChild(imgContainer);
+        imgContainer.appendChild(img);
+        a.appendChild(h2);
+        article.appendChild(p1);
+        article.appendChild(p2);
+        article.appendChild(p3);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,49 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+async function getPhotographers() {
+    const response = await fetch("data/photographers.json");
+    let photographers = "";
+
+    if (response.ok) { // if HTTP-status is 200-299
+        // get the response body
+        photographers = await response.json();
+    }
+    else {
+        alert("HTTP-Error: " + response.status);
+    }
+    console.log(photographers)
+    // et bien retourner le tableau photographers seulement une fois récupéré
+    return (photographers)
+}
+
+async function displayData(photographers) {
+    //récupérer l'id en paramètre de l'url
+    const urlParams = new URLSearchParams(window.location.search);
+    const currentPhotographerId = urlParams.get('id');
+    //sélectionner les éléments du dom à modifier
+    phName = document.querySelector('#ph-name');
+    phCity = document.querySelector('#ph-city');
+    phTagline = document.querySelector('#ph-tagline');
+    phPic = document.querySelector('#ph-pic');
+
+    photographers.forEach((photographer) => {
+        //récupérer les données du photographe
+        const { name, portrait, id, city, country, tagline, price } = photographer;
+        //sélectionner celui dont l'id est en paramètre de l'url
+        if (id == currentPhotographerId) { //que 2 "==" car les variables ne sont pas du même type
+            phName.textContent = name;
+            phCity.textContent = city+', '+country;
+            phTagline.textContent = tagline;
+            phPic.setAttribute('src', 'assets/photographers/'+portrait);
+            phPic.setAttribute('alt', 'Portrait de '+name);
+        }
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
+
+init();
