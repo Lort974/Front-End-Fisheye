767643a2acbebc008645074c1980b33f88a2967e
Laurent Bigey <laurent.bigey@outlook.fr>
2023-06-30 23:26:05 +0200

    creation de la lightbox et navigation à la souri

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -48,7 +48,7 @@ function photographerPageFactory(data) { //factory des détails du photographe s
 }
 
 function achievementsFactory(data) { //factory des réalisations du photographe sur sa page
-    const { title, image, video, id } = data;
+    const { title, image, video, id, date, likes } = data;
     //récupérer le nom du dossier contenant les médias du photographe, qui correspond à son prénom
     const photographerName = document.querySelector('.photographer-summary > h1').textContent;
     const photographerFirstName = photographerName.split(' ')[0];
@@ -57,7 +57,6 @@ function achievementsFactory(data) { //factory des réalisations du photographe 
         
         const article = document.createElement( 'article' );
         article.setAttribute('id', id);
-        //CREER UNE FONCTION POUR RANGER LES MEDIA PAR ORDRE CROISSANT D'ID POUR FACILITER LE DEFILEMENT DANS LA LIGHTBOX
         const mediaContainer = document.createElement( 'div' );
         mediaContainer.setAttribute('class', 'media-container');
         let media;
@@ -65,6 +64,9 @@ function achievementsFactory(data) { //factory des réalisations du photographe 
             media = document.createElement('img');
             media.setAttribute('src', 'assets/photographers/'+photographerFirstName+'/'+image); //trouver comment récupérer le nom du photographe pour l'intégrer au chemin d'accès à l'image
             media.setAttribute('alt', 'Photo intitulée "'+title+'" de '+photographerName);
+            media.setAttribute('data-title', title);
+            media.setAttribute('data-date', date);
+            media.setAttribute('data-likes', likes);
             mediaContainer.appendChild(media);
             mediaContainer.setAttribute('data', 'image'); //reconnaître via CSS si c'est une image ou une vidéo
             //régler les dimensions en fonction du ratio hauteur/largeur
@@ -79,6 +81,9 @@ function achievementsFactory(data) { //factory des réalisations du photographe 
         if (video) {
             media = document.createElement('video');
             media.setAttribute('controls', 'true');
+            media.setAttribute('data-title', title);
+            media.setAttribute('data-date', date);
+            media.setAttribute('data-likes', likes);
             mediaContainer.appendChild(media);
             mediaContainer.setAttribute('data', 'video'); //reconnaître via CSS si c'est une image ou une vidéo
             source = document.createElement('source');
@@ -132,7 +137,136 @@ async function displayData(photographers, media) { //fonction qui appelle les fa
             mediaSection.appendChild(getMediaDOM);
         }
     });
+    //par défaut, tri par ordre alphabétique
+    sortMedias('title');
 };
+
+//trier les medias
+function sortMedias(method) {
+    const medias = document.querySelectorAll('.media-section > article .media-container > *');
+    let mediaTable = [];
+    if (method === 'title') { //tri par ordre alphabétique
+        //créer un tableau des titres
+        medias.forEach(e => {
+            const title = e.getAttribute('data-title');
+            mediaTable.push(title);
+        });
+        //le ranger dans l'ordre alphabétique
+        mediaTable.sort();
+        //parcourir ce tableau et donner au media dont le data-title match avec l'occurence l'order correspondant à l'index
+        for (let i = 0; i < mediaTable.length; i++) {
+            const element = mediaTable[i]; //titre du media
+            media = document.querySelector('*[data-title="'+element+'"]'); //media dont le titre est element
+            media.parentNode.parentNode.style.order = i; //donner à l'article parent la propriété order = rang alphabétique (i)
+            media.setAttribute('data-order', i); //stocker cet ordre dans le media
+        }
+    }
+}
+
+//afficher le média au clic sur la miniature et permettre la navigation
+async function lightboxMediaDisplay(idMedia, media) {
+    media.forEach((achievement) => {
+        const { title, id, image, video, photographerId } = achievement;
+        const mediaContainer = document.getElementById('media-container');
+        //récupérer le nom du dossier contenant les médias du photographe, qui correspond à son prénom
+        const photographerName = document.querySelector('.photographer-summary > h1').textContent;
+        const photographerFirstName = photographerName.split(' ')[0];
+        //récupérer l'id en paramètre de l'url
+        const urlParams = new URLSearchParams(window.location.search);
+        const currentPhotographerId = urlParams.get('id');
+
+        //sortir tous les medias du photographe
+        if (photographerId == currentPhotographerId) {
+            let closeUpMedia = '';
+            if (image) {
+                closeUpMedia = document.createElement('img');
+                closeUpMedia.setAttribute('src', 'assets/photographers/'+photographerFirstName+'/'+image); //trouver comment récupérer le nom du photographe pour l'intégrer au chemin d'accès à l'image
+                closeUpMedia.setAttribute('alt', 'Photo intitulée "'+title+'" de '+photographerName);
+                closeUpMedia.setAttribute('data-id', id);
+                mediaContainer.appendChild(closeUpMedia);
+                mediaContainer.setAttribute('data', 'image'); //reconnaître via CSS si c'est une image ou une vidéo
+                //régler les dimensions en fonction du ratio hauteur/largeur
+                const ratio = closeUpMedia.height/closeUpMedia.width;
+                if (ratio < 1) {
+                    closeUpMedia.setAttribute('style', 'max-height: 800px; max-width: unset;');
+                }
+                else {
+                    closeUpMedia.setAttribute('style', 'max-height: unset; max-width: 950px;');
+                }
+            }
+            if (video) {
+                closeUpMedia = document.createElement('video');
+                closeUpMedia.setAttribute('controls', 'true');
+                closeUpMedia.setAttribute('data-id', id);
+                mediaContainer.appendChild(closeUpMedia);
+                mediaContainer.setAttribute('data', 'video'); //reconnaître via CSS si c'est une image ou une vidéo
+                source = document.createElement('source');
+                source.setAttribute('src', 'assets/photographers/'+photographerFirstName+'/'+video);
+                source.setAttribute('type', 'video/mp4');
+                closeUpMedia.appendChild(source);
+                mediaContainer.setAttribute('style', 'max-height: unset; max-width: 950px;');
+            }
+            //n'afficher sur celui qui a été cliqué
+            if (id == idMedia) {
+                closeUpMedia.style.display = 'block';
+                //définir data-display pour la fonction lightboxBrowse()
+                closeUpMedia.setAttribute('data-display', 'true');
+            }
+            else {
+                closeUpMedia.style.display = 'none';
+                //définir data-display pour la fonction lightboxBrowse()
+                closeUpMedia.setAttribute('data-display', 'false');
+            }
+        }
+    });
+};
+
+//naviguer dans la lightbox
+function lightboxBrowse(direction) {
+    //définir l'opérateur qui calculera le nouveau rang à afficher
+    let operator = 0;
+    if (direction === 'previous') {
+        operator--;
+    }
+    else if (direction === 'next') {
+        operator++;
+    }
+    //trouver le rang du media actuellement affiché
+    const currentMedia = document.querySelector('.lightbox-media-container > *[data-display="true"]');
+    const currentMediaId = currentMedia.getAttribute('data-id');
+    const article = document.querySelector('.media-section article[id="'+currentMediaId+'"]');
+    const currentRank = parseInt(article.style.order);
+    //trouver le nouveau rang à afficher
+    let newRank = currentRank + operator;
+    const mediasList = document.querySelectorAll('.media-section article');
+    const mediasNbr = mediasList.length;
+    //si le nouveau rang à afficher est supérieur au nombre de médias, on revient à 0
+    if (newRank >= mediasNbr) {
+        newRank = 0;
+    }
+    //s'il est inférieur à 0, on reprend à la fin
+    else if (newRank < 0) {
+        newRank = mediasNbr-1;
+    }
+    let newMediaId = '';
+    mediasList.forEach(e => {
+        if (e.style.order == newRank) {
+            newMediaId = e.getAttribute('id');
+        }
+    });
+    //afficher le media dans la direction cliquée
+    const closeUpMediasList = document.querySelectorAll('.lightbox-media-container > *');
+    closeUpMediasList.forEach(e => {
+        if (e.getAttribute('data-id') == newMediaId) {
+            e.style.display = 'block';
+            e.setAttribute('data-display', 'true');
+        }
+        else {
+            e.style.display = 'none';
+            e.setAttribute('data-display', 'false');
+        }
+    });
+}
 
 async function init() {
     // Récupère les datas des photographes
@@ -143,8 +277,10 @@ async function init() {
     const lightbox = document.getElementById('lightbox');
     const lightboxOpener = document.querySelectorAll('.media-section > article');
     lightboxOpener.forEach(element => {
+        const idMedia = element.getAttribute('id');
         element.addEventListener('click', event => {
             lightbox.style.display = 'flex';
+            lightboxMediaDisplay(idMedia, media);
             document.querySelector('body').classList.add('no-scroll');
         })
     });
@@ -157,4 +293,23 @@ document.getElementById('close-lightbox').addEventListener('click', e => {
     e.preventDefault();
     document.getElementById('lightbox').style.display = 'none';
     document.querySelector('body').classList.remove('no-scroll');
+    //supprimer le html des medias
+    const images = document.querySelectorAll('#media-container img');
+    images.forEach(e => {
+        e.remove();
+    });
+    const videos = document.querySelectorAll('#media-container video');
+    videos.forEach(e => {
+        e.remove();
+    });
+})
+
+//appel de la navigation dans la lightbox
+document.getElementById('lightbox-previous').addEventListener('click', e => {
+    e.preventDefault();
+    lightboxBrowse('previous');
+})
+document.getElementById('lightbox-next').addEventListener('click', e => {
+    e.preventDefault();
+    lightboxBrowse('next');
 })
